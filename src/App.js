3df1ececfd2332281cdb09c37abd809cc3ecd479
oagoulzi <oagoulzi@lnet.ma>
2022-02-28 11:24:23 +0100

    finished styling the navbar

--- src/App.js
+++ src/App.js
@@ -36,10 +36,20 @@ function App() {
 				</setLangContext.Provider>
 			</langContext.Provider>
 			<header>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellat nisi maxime error tenetur molestias ipsa eius cupiditate sed veniam aspernatur aliquam ducimus nesciunt totam soluta, hic animi id suscipit dicta. Ipsum dolorum vel cumque officiis quibusdam? Dignissimos, temporibus? Ab quasi, dolor eaque repudiandae impedit incidunt dignissimos dolores aperiam odio quae ipsum iste. Nemo sunt aliquid neque fuga sequi obcaecati perferendis cumque animi a dolores quo, eveniet adipisci, culpa vero. Illo obcaecati voluptatibus odio quasi doloremque magni debitis sint. Cumque velit omnis error voluptatem sequi tempora vel aliquid, eum cupiditate et, ad optio odio ullam soluta tempore saepe iste assumenda obcaecati expedita recusandae deserunt voluptates! Tempora molestiae unde consectetur. Aspernatur quo veniam ullam distinctio, ipsam corporis nam. Labore tenetur error sunt eos, magnam magni, voluptas ab a sed, dolor neque tempora asperiores voluptatibus soluta illum ea minus tempore deserunt ullam ex? Porro tempora harum ab sed alias earum doloribus voluptatibus repellat atque, a, autem nisi id sunt assumenda voluptatem aliquam accusantium mollitia molestias dolorem iusto iste dolorum! Eaque magni, quisquam at tempora quam explicabo doloribus quos dolorem obcaecati, minus sunt asperiores nemo incidunt architecto nesciunt maxime ad fuga illum ab optio illo laudantium, aspernatur vel? Voluptas rerum illum esse maxime ea non inventore ipsam temporibus voluptatum, iure odio quidem quasi velit soluta asperiores dolore perferendis assumenda impedit, dicta id aliquam ad cum laboriosam! Deleniti sit aliquam quas commodi fugiat, illo tenetur ipsum dolorum beatae exercitationem placeat possimus delectus impedit voluptatem cumque doloremque nesciunt? Adipisci quis repellat ratione aliquid! Molestias nam doloremque fugiat perspiciatis accusamus. Nemo at ducimus autem sint amet exercitationem sed doloribus enim dicta aliquam voluptatum, hic quidem minima reiciendis nesciunt. Sint minus ab numquam eligendi. Similique aperiam vitae deserunt velit rem voluptatibus ad, beatae eum consequatur dolore dolorem recusandae eaque sapiente ipsa corrupti! Quisquam cumque eaque velit veniam eligendi voluptates harum voluptatum soluta a, illo cupiditate amet tempore itaque perferendis commodi maxime. Amet labore natus porro explicabo ipsa, deleniti delectus corporis provident magni deserunt ut beatae fuga possimus, voluptatem, consectetur temporibus aperiam ratione culpa. Labore perspiciatis magni doloremque dolores laboriosam ratione modi in fugiat mollitia. Ullam deleniti illo possimus iste vitae. Expedita doloremque nam consequatur vel voluptatum adipisci doloribus perspiciatis fugit beatae repellendus aliquam deleniti quae, quo quibusdam sed in? Qui, repellat error dolorum nostrum explicabo cumque ipsum facilis! Fugit, quo. Quod laudantium modi omnis porro ab nobis facilis vitae possimus doloribus. Esse quos itaque id architecto, cupiditate, sequi quam molestias laudantium, tempora quasi dolores ducimus repellat modi asperiores? Iusto repellendus in neque assumenda quos, laboriosam rerum et! Atque dolor ex a veniam! Illum nulla ullam corporis doloremque animi non quis veniam repellat! Dolores iusto, libero consectetur harum iste deleniti, officia, tempora culpa reiciendis quia quos facere beatae? Minima esse maiores nam perferendis ex magni, laboriosam magnam quia unde, assumenda nostrum ratione molestias, cum minus. Doloribus illo perspiciatis neque quis ad delectus, quod aliquam esse dolorem dolore, praesentium excepturi eaque rerum, quo distinctio? Nihil officiis necessitatibus praesentium in, iste beatae inventore nam provident quasi eaque dolore distinctio ex?
+				<div className="container">
+					<h1>{headerContent.title[lang]}</h1>
+					
+				</div>
 			</header>
 		</div>
 	);
+}
+
+const headerContent = {
+	title: {
+		en: "Title",
+		ar: "عنوان"
+	},
 }
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,54 +1,156 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { FaAngleDown, FaBars } from 'react-icons/fa'
 import { langContext } from '../App'
 import LangSwitcher from './langSwitcher'
 import ThemeSwitcher from './themeSwitcher'
 
-const links = [
+import image1 from "../images/image1.png"
+import image2 from "../images/image2.png"
+
+const dropdownContent = [
     {
-        id: 1,
-        ar: "الرئيسية",
-        en: "Home",
+        en: "Taking action",
+        ar: "تصرف"
     },
     {
-        id: 2,
-        ar: "المنتج",
-        en: "Product",
+        en: "My impact",
+        ar: "تأثيري"
     },
     {
-        id: 3,
-        ar: "المزايا",
-        en: "Features",
+        en: "Carbon compensation",
+        ar: "تعويض الكربون"
     },
     {
-        id: 4,
-        ar: "التسعير",
-        en: "Pricing",
-    }
+        en: "Pachama's impact",
+        ar: "تأثير الباشاما"
+    },
+    {
+        en: "we're helping the world",
+        ar: "نحن نساعد العالم"
+    },
+    {
+        en: <>Tonnes of CO<sub>2</sub> Compensated</>,
+        ar: "أطنان من ثاني أكسيد الكربون تم تعويضها"
+    },
 ]
 
 const Navbar = () => {
-
     const lang = useContext(langContext)
 
-    return (
-        <nav>
-            <div className="container">
-                <h1 className="logo">
-                    Logo
-                </h1>
-                <div className="links">
-                    {console.log(links, links[0][lang])}
-                    {
-                        links.map(link => (
-                            <span className="link" key={link.id}>
-                                {link[lang]}
-                            </span>
-                        ))
-                    }
+    const links = [
+        {
+            id: 1,
+            ar: "الرئيسية",
+            en: "Home",
+            active: true
+        },
+        {
+            id: 2,
+            ar: "المنتج",
+            en: "Product",
+        },
+        {
+            id: 3,
+            ar: "التأثير",
+            en: "Impact",
+            children: (
+                <div className='dropdown'>
+                    <div>
+                        <h5>{dropdownContent[0][lang]}</h5>
+                        <div className="impact">
+                            <div className="image">
+                                <img src={image1} alt="" />
+                            </div>
+                            <div className="content">
+                                <h6>{dropdownContent[1][lang]}</h6>
+                                <p>{dropdownContent[2][lang]}</p>
+                            </div>
+                        </div>
+                        <div className="impact">
+                            <div className="image">
+                                <img src={image2} alt="" />
+                            </div>
+                            <div className="content">
+                                <h6>{dropdownContent[3][lang]}</h6>
+                                <p>{dropdownContent[4][lang]}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <span>5972</span>
+                        <span>{dropdownContent[5][lang]}</span>
+                    </div>
                 </div>
-                <div className="switchers">
-                    <ThemeSwitcher />
-                    <LangSwitcher />
+            )
+        },
+        {
+            id: 4,
+            ar: "التسعير",
+            en: "Pricing",
+        },
+        {
+            id: 5,
+            ar: "الدخول",
+            en: "Sign in",
+        },
+        {
+            id: 6,
+            ar: "التسجيل",
+            en: "Register",
+            primary: true
+        }
+    ]
+    
+    const [show, setShow] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
+    const navFoot = useRef()
+
+    useEffect(() => {
+        window.onscroll = () => {
+            if(window.scrollY !== 0)
+                setScrolled(true)
+            else
+                setScrolled(false)
+        }
+    }, [])
+
+
+    return (
+        <nav className={`${scrolled? 'scrolled' : ''}`}>
+            <div className="container">
+                <div className="nav-head">
+                    <h1 className="logo">
+                        Logo
+                    </h1>
+                    <button onClick={() => setShow(!show)} aria-label='show or hide navbar links in mobile'>
+                        <FaBars />
+                    </button>
+                </div>
+                <div ref={navFoot} className={`nav-foot ${show? 'showed' : ""}`}>
+                    <div className="links">
+                        {
+                            links.map(link => {
+                                if(link.children){
+                                    return (
+                                        <span className={`link ${link.children? 'has-ch' : ""} ${link.active? 'active' : ""} ${link.primary? 'primary' : ""}`} key={link.id}>
+                                            {link[lang]}
+                                            <FaAngleDown/>
+                                            {link.children}
+                                        </span>
+                                    )
+                                }
+                                return (
+                                    <a href="/" className={`link ${link.children? 'has-ch' : ""} ${link.active? 'active' : ""} ${link.primary? 'primary' : ""}`} key={link.id}>
+                                        {link[lang]}
+                                    </a>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="switchers">
+                        <ThemeSwitcher />
+                        <LangSwitcher />
+                    </div>
                 </div>
             </div>
         </nav>
